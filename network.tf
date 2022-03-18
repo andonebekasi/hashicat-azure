6fe0b4debed2d8d09cb0fa4e280bd8a5c4692136
andi <34510976+andonebekasi@users.noreply.github.com>
2022-03-19 01:43:21 +0700

    Update network.tf

--- network.tf
+++ network.tf
@@ -1,3 +1,12 @@
+resource "azurerm_resource_group" "myresourcegroup" {
+  name     = "${var.prefix}-workshop"
+  location = var.location
+
+  tags = {
+    environment = "Production"
+  }
+}
+
 module "network" {
   source  = "app.terraform.io/jasalinux/network/azurerm"
   version = "3.5.0"
